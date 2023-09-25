195e86aa5e0f368ecbe0b78a79c61e19a0988931
marwagbs <90385255+marwagbs@users.noreply.github.com>
2023-09-25 20:56:00 +0200

    Create BookingType.php

---
+++ booking/src/Form/BookingType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('startDateAt')
+            ->add('endDateAt', DateType::class, [
+                'widget' => 'single_text',
+                'input' =>'datetime_immutable'
+            ])
+
+            ->add('nbCustomer')
+            ->add('price')
+           
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
